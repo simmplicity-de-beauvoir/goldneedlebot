0c7a1eeef75fb460b2221d3b0b62702fb00025e7
simmplcity <simm@simmpli.city>
2023-05-23 20:33:56 -0400

    fix time calc

--- helpers.py
+++ helpers.py
@@ -7,12 +7,14 @@ async def timestr_to_num(time_string):
 	if time_string.isnumeric():
 		return int(time_string)
 	# check that string is valid
-	if re.match(r'[0-9]+[mdw]',time_string) is None:
+	if re.match(r'[0-9]+[mdhw]',time_string) is None:
 		return None
 	if time_string.endswith('m'):
 		return int(time_string.strip('m')) * 60
+	if time_string.endswith('h'):
+		return int(time_string.strip('h')) * 60 * 60
 	if time_string.endswith('d'):
-		return int(time_string.strip('d')) * 60 * 24
+		return int(time_string.strip('d')) * 60 * 60 * 24
 	if time_string.endswith('w'):
-		return int(time_string.strip('w')) * 60 * 24 * 7
+		return int(time_string.strip('w')) * 60 * 60 * 24 * 7
 
